ff04b36b8fdb1edcf74cffaf7f8df843d7a49d73
SORDE12 <sordeomkar2564@gmail.com>
2022-12-16 10:38:13 +0530

    added

--- project/src/Components/Navbar/Navbar.jsx
+++ project/src/Components/Navbar/Navbar.jsx
@@ -1,59 +1,68 @@
 import './Navbar.css'
 import React from 'react'
-import { Box, Text, Flex, Image, Input, Spacer, InputGroup } from '@chakra-ui/react'
+import { Box, Text, Flex, Image, Input, Spacer, InputGroup,Grid,GridItem, Button } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import img from "../images/softSoul.png"
+import { ChevronDownIcon } from "@chakra-ui/icons"
 
 export const Navbar = () => {
     return (
         <div>
-            <Box bg='gray' w='100%'  color='#21201E' PaddingRight={20} paddingLeft={20}>
-                <Flex gap={2} paddingLeft={2} paddingRight={2} >
+            <Box bg='whiteSmoke' w='100%' color='#21201E' >
+                <Flex gap={2}>
                     <Image borderRadius='full' p={2} boxSize='10%' src={img} alt='logo' />
-                    <Text as='b' color="white" p={25} fontSize='30px'>Soft Soul</Text>
                     <div class="dropdown">
-                        <p class="dropbtn">Software</p>
+                        <p class="dropbtn">Software{<ChevronDownIcon />}</p>
                         <div class="dropdown-content" >
-                            <h3 style={{marginTop:"50px"}}>The HubSpot CRM Platform</h3>
+                            <h3 style={{ marginTop: "50px" , color:"#2E2E33"}}>The HubSpot CRM Platform</h3>
+                            <p style={{ marginTop: "10px" , color:"gray"}}>All of HubSpot’s marketing, sales CRM, customer service, CMS, and operations software on one platform.</p>
+                            <Grid style={{marginBottom:"15px",marginTop:"15px"}} templateColumns='repeat(2, 1fr)' gap={4}>
+                                {/* <GridItem colSpan={2} h='10'gap bg='tomato' /> */}
+                                
+                                <GridItem><Button color="blue" backgroundColor="white">Free HubSpot CRM</Button></GridItem>
+                                <GridItem ><Button color="blue" backgroundColor="white">Overview of all products</Button></GridItem>
+                            </Grid>
+                            <Grid marginTop="15px" templateColumns='repeat(3, 1fr)' gap={4}>
+                                {/* <GridItem colSpan={2} h='10'gap bg='tomato' /> */}
+                                <GridItem>1</GridItem>
+                                <GridItem>1</GridItem>
+                                <GridItem>1</GridItem>
+                                <GridItem>1</GridItem>
+                                <GridItem>1</GridItem>
+                            </Grid>
+                        </div>
+                    </div>
+                    <Link to="/pricing" style={{ color: "black", fontSize: "20px", margin: "auto", textUnderlineOffset: "5px" }}><p>Pricing</p></Link>
+                    <div class="dropdown">
+                        <p class="dropbtn">Resources{<ChevronDownIcon />}</p>
+                        <div class="dropdown-content" >
+                            <h3 style={{ marginTop: "50px" }} >The HubSpot CRM Platform</h3>
                             <p>All of HubSpot’s marketing, sales CRM, customer service, CMS, and operations software on one platform.</p>
                             <a href="#">Link 1</a>
                             <a href="#">Link 2</a>
                             <a href="#">Link 3</a>
                         </div>
                     </div>
-                    <Link to="/pricing" style={{ color: "white",fontSize: "20px",margin:"auto", textUnderlineOffset: "5px"}}><p>Pricing</p></Link>
-                    <div class="dropdown">
-                        <p class="dropbtn">Resources</p>
-                        <div class="dropdown-content" >
-                            <h3 style={{marginTop:"50px"}} >The HubSpot CRM Platform</h3>
+                    <Spacer />
+
+                    <div class="dropdown1">
+                        <p class="dropbtn1" style={{ paddingRight: "50px" }}>About{<ChevronDownIcon />}</p>
+                        <div class="dropdown-content1" >
+                            <h3 style={{ marginTop: "50px" }}>The HubSpot CRM Platform</h3>
                             <p>All of HubSpot’s marketing, sales CRM, customer service, CMS, and operations software on one platform.</p>
                             <a href="#">Link 1</a>
                             <a href="#">Link 2</a>
                             <a href="#">Link 3</a>
                         </div>
-                    </div>
-                   <Spacer/>
-                   
-                    <div style={{display:Flex,justifyContent:"center",alignItems:"center",margin:"auto"}}>
-                    <Link to="/login" style={{ color: "white",fontSize: "20px",marginRight:"200px",margin:"auto",textUnderlineOffset: "5px"}}><p>Login</p></Link>
-                    
-                    </div>
-                    
-                    <div style={{display:Flex,justifyContent:"center",alignItems:"center",margin:"auto"}}>
-                    <Link to="/login" style={{ color: "white",fontSize: "20px",marginRight:"200px",margin:"auto",textUnderlineOffset: "5px"}}><p>Customer Support</p></Link>
                     </div>
 
-                    
+                    <div style={{ display: Flex, justifyContent: "center", alignItems: "center", margin: "auto" }}>
+                        <Link to="/login" style={{ color: "black", fontSize: "20px", margin: "auto", textUnderlineOffset: "5px" }}><p style={{ paddingRight: "50px" }}>Customer Support</p></Link>
+                    </div>
 
-                    <div class="dropdown">
-                        <p class="dropbtn">Software</p>
-                        <div class="dropdown-content" >
-                            <h3 style={{marginTop:"50px"}}>The HubSpot CRM Platform</h3>
-                            <p>All of HubSpot’s marketing, sales CRM, customer service, CMS, and operations software on one platform.</p>
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
+                    <div style={{ display: Flex, justifyContent: "center", alignItems: "center", margin: "auto" }}>
+                        <Link to="/login" style={{ color: "black", fontSize: "20px", margin: "auto", textUnderlineOffset: "5px" }}><p style={{ paddingRight: "50px" }}>Login</p></Link>
+
                     </div>
 
                 </Flex>
